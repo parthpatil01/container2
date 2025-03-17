54fa5bb7502a45dbc75abd4366c06ad4b07e27a4
parthpatil01 <patilvparth15@gmail.com>
2025-03-17 19:49:50 -0300

    final update

--- calculator.js
+++ calculator.js
@@ -10,39 +10,39 @@ const PV_DIR = '/parth_PV_dir';
 
 // Endpoint to calculate product total
 app.post('/calculate', (req, res) => {
-    return res.status(200).json({ message: 'we are in container 2.' });
-    // const { file, product } = req.body;
+   
+    const { file, product } = req.body;
 
-    // if (!file || !product) {
-    //     return res.status(400).json({ file: null, error: 'Invalid JSON input.' });
-    // }
+    if (!file || !product) {
+        return res.status(400).json({ file: null, error: 'Invalid JSON input.' });
+    }
 
-    // const filePath = path.join(PV_DIR, file);
+    const filePath = path.join(PV_DIR, file);
 
-    // if (!fs.existsSync(filePath)) {
-    //     return res.status(404).json({ file, error: 'File not found.' });
-    // }
+    if (!fs.existsSync(filePath)) {
+        return res.status(404).json({ file, error: 'File not found.' });
+    }
 
-    // // Check if the file has a .csv extension
-    // if (path.extname(filePath).toLowerCase() !== '.csv') {
-    //     return res.status(400).json({ file, error: 'Input file not in CSV format.' });
-    // }
+    // Check if the file has a .csv extension
+    if (path.extname(filePath).toLowerCase() !== '.csv') {
+        return res.status(400).json({ file, error: 'Input file not in CSV format.' });
+    }
 
-    // const results = [];
-    // fs.createReadStream(filePath)
-    //     .pipe(csv({ headers: ['product', 'amount'], skipLines: 0 }))
-    //     .on('data', (data) => results.push(data))
-    //     .on('end', () => {
-    //         try {
-    //             const total = results
-    //                 .filter((row) => row.product === product)
-    //                 .reduce((sum, row) => sum + parseInt(row.amount), 0);
+    const results = [];
+    fs.createReadStream(filePath)
+        .pipe(csv({ headers: ['product', 'amount'], skipLines: 0 }))
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            try {
+                const total = results
+                    .filter((row) => row.product === product)
+                    .reduce((sum, row) => sum + parseInt(row.amount), 0);
 
-    //             res.json({ file, sum: total });
-    //         } catch (error) {
-    //             res.status(400).json({ file, error: 'Input file not in CSV format.' });
-    //         }
-    //     });
+                res.json({ file, sum: total });
+            } catch (error) {
+                res.status(400).json({ file, error: 'Input file not in CSV format.' });
+            }
+        });
 });
 
 const PORT = 5001;
